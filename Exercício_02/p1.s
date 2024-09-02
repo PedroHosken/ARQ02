a5d9b16830c2d867b767d78d982974a0d58ca901
Pedro Hosken <127361258+PedroHosken@users.noreply.github.com>
2024-09-02 08:30:19 -0300

    Exercicios 02

---
+++ Exercício_02/p1.s
@@ -0,0 +1,11 @@
+addi s1, zero, 2
+addi s2, zero, 3
+addi s3, zero, 4
+addi s4, zero, 5
+add t0, s1, s2
+add t1, s3, s4
+sub s5, t0, t1
+add t2, s2, s5
+sub s6, s1, t2
+sub s2, s5, s6
+nop
---
+++ Exercício_02/p2.s
@@ -0,0 +1,4 @@
+/*add s1, zero, 1
+addi t0, zero, 5
+addi t1, s1, 15
+sub s2, t0, t1 */
