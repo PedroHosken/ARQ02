1fc58bc792911145add1ac01ab47249018026fbb
Pedro Hosken <127361258+PedroHosken@users.noreply.github.com>
2024-11-18 08:22:26 -0300

    Código correto

--- Exercício_05/SomaV.c
+++ Exercício_05/SomaV.c
@@ -16,10 +16,15 @@ void somar_vetor(int *vetor, int tamanho)
 int main()
 {
     int vetor[tamanho];
+    double start, end;
+    omp_set_num_threads(1); // Define 4 threads
     for (int i = 0; i < tamanho; i++)
     {
         vetor[i] = i + 1;
     }
+    start = omp_get_wtime();
     somar_vetor(vetor, tamanho);
+    end = omp_get_wtime();
+    printf("Tempo : %lf", end - start);
     return 0;
 }
